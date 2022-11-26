4861f4e31d99bfc020ab3986e43ca2a3bb420e41
moehr-moehr <bigpandacomics.com@gmail.com>
2022-11-26 18:20:58 -0500

    deletes unnecessary backup of src
    first working version
    may need a fix for graphics bug where some item rooms occasionally are missing a column of tiles

--- SRC3/tilesets/solidityValues.asm
+++
@@ -1,9 +0,0 @@
-; 8:7EFA - Solidity threshholds
-    db $69, $63, $6e, $ff ; 0 - plantBubbles
-    db $5e, $54, $5e, $ff ; 1 - ruinsInside
-    db $f0, $f0, $f0, $ff ; 2 - queen
-    db $63, $5d, $63, $ff ; 3 - caveFirst
-    db $69, $69, $69, $ff ; 4 - surface
-    db $42, $42, $42, $ff ; 5 - lavaCaves
-    db $5c, $54, $64, $ff ; 6 - ruinsExt
-    db $75, $75, $75, $ff ; 7 - finalLab
